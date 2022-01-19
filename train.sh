aa1f8dd5ebd28cbb788a42045f40704c6d593b81
Eunchong Kim <rha2thm@gmail.com>
2022-01-19 15:27:53 +0900

    changed train configs

--- train.sh
+++ train.sh
@@ -4,8 +4,8 @@ python train.py --model rapid_pL1 \
                 --backbone yolov5x \
                 --dataset COCO \
                 --batch_size 8 \
-                --eval_interval 20 \
-                --img_interval 100 \
-                --checkpoint_interval 100
+                --eval_interval 1000 \
+                --img_interval 1000 \
+                --checkpoint_interval 5000
                 
 
